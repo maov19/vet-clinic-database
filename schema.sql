32bae5e68fc2a7bc033bb630a5707750f12fd7f8
mossa <martin.ossa.8@gmail.com>
2023-05-04 16:33:29 +0200

    Add structure for specilizations and visits tables

--- schema.sql
+++ schema.sql
@@ -1,12 +1,18 @@
 /* Database schema to keep the structure of entire database. */
 
 -- Create the database
-CREATE DATABASE ;
+CREATE DATABASE vet_clinic;
 
 -- Switch to the database
 \c 
 
--- Create the animals table
+-- Create the vets table
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    age INTEGER,
+    date_of_graduation DATE
+);
 
 -- Create owners table
 CREATE TABLE owners (
@@ -32,6 +38,23 @@ weight_kg DECIMAL(5, 2),
 species_id INTEGER REFERENCES species(id),
 owner_id INTEGER REFERENCES owners(id)
 );
+
+CREATE TABLE specializations (
+  vet_id INT,
+  species_id INT,
+  FOREIGN KEY (vet_id) REFERENCES vets(id),
+  FOREIGN KEY (species_id) REFERENCES species(id)
+);
+
+CREATE TABLE visits (
+  id INT PRIMARY KEY,
+  animal_id INT,
+  vet_id INT,
+  visit_date DATE,
+  FOREIGN KEY (animal_id) REFERENCES animals(id),
+  FOREIGN KEY (vet_id) REFERENCES vets(id)
+);
+
 
 
 
