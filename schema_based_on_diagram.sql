91a9029a86ee23711a75a0699f1bf1174b5280bd
mossa <martin.ossa.8@gmail.com>
2023-05-09 10:05:19 +0200

    Add schema file

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,40 @@
+CREATE TABLE patients (
+id SERIAL PRIMARY KEY,
+name VARCHAR ( 50 ) UNIQUE NOT NULL,
+date_of_birth DATE
+);
+
+CREATE TABLE medical_histories (
+	id SERIAL PRIMARY KEY,
+	admitted_at TIMESTAMP NOT NULL,
+	status VARCHAR NOT NULL,
+ FOREIGN KEY patients_id INT REFERENCES patients(id)
+);
+
+CREATE TABLE treatments (
+	id  SERIAL PRIMARY KEY,
+	type VARCHAR,
+	name VARCHAR NOT NULL
+);
+
+CREATE TABLE medical_treatments(
+  medical_histories_id INT REFERENCES medical_histories(id),
+  treatments_id INT REFERENCES treatments(id)
+)
+
+CREATE TABLE invoices (
+	id  SERIAL PRIMARY KEY,
+	total_amount DECIMAL,
+	generated_at TIMESTAMP,
+	payed_at TIMESTAMP,
+	FOREIGN KEY medical_history__id INT REFERENCES medical_histories(id),
+);
+
+CREATE TABLE invoice_items (
+	id  SERIAL PRIMARY KEY,
+	unit_price DECIMAL,
+	quantity INT,
+	total_price DECIMAL,
+	FOREIGN KEY invoice_id INT REFERENCES invoices(id),
+	FOREIGN KEY treatment_id INT REFERENCES treatments(id),
+);
